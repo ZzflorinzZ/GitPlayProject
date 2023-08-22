e94e66cdb8ed40240edefd95bc915f1f62ba96a9
Ispas Florian <ispas.florian@gmail.com>
2023-08-22 19:41:28 +0300

    modif local 6

--- src/test/TestGit.java
+++ src/test/TestGit.java
@@ -2,23 +2,23 @@ package test;
 
 public class TestGit {
 
-  //cod adaugat de coleg	
+  //cod adaugat de coleg	+++++++++++
 	
-	//cod adaugat local
+	//cod adaugat local		______________
 	
-  //cod adaugat de coleg 2	
+  //cod adaugat de coleg 2	=============
 	
 	//linie noua cod 3
 	
 	//linie cod remote
 	
-	//linie noua cod 4
+	//linie noua cod 4		//////////////////
 	
 //linie cod remote 4
 	
 	//linie noua cod 5
 
-	//linie cod remote 5
+	//linie cod remote 5	???????????
 
 
 }
