632e549d3e116cf54b3e75434c289dc9f2aabc7e
ZzflorinzZ <128520451+ZzflorinzZ@users.noreply.github.com>
2023-08-22 19:36:07 +0300

    Update TestGit.java

--- src/test/TestGit.java
+++ src/test/TestGit.java
@@ -3,12 +3,20 @@ package test;
 public class TestGit {
 
   //cod adaugat de coleg	
+	
 	//cod adaugat local
+	
   //cod adaugat de coleg 2	
+	
 	//linie noua cod 3
+	
 	//linie cod remote
+	
 	//linie noua cod 4
+	
 //linie cod remote 4
+
+	//linie cod remote 5
 
 
 }
