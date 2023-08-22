8b0a8a3e7d25e1d8e82e6cdd9eb7910ddbb749c5
Ispas Florian <ispas.florian@gmail.com>
2023-08-22 19:29:46 +0300

    cod local 4

--- src/test/TestGit.java
+++ src/test/TestGit.java
@@ -2,19 +2,12 @@ package test;
 
 public class TestGit {
 
-  //cod adaugat de coleg
-
-	
+  //cod adaugat de coleg	
 	//cod adaugat local
-	
-
-  //cod adaugat de coleg 2
-	
-	
+  //cod adaugat de coleg 2	
 	//linie noua cod 3
-
-
 	//linie cod remote
+	//linie noua cod 4
 
 
 }
