59207ad2062e2047d3ac164a557c782974e9d626
Ispas Florian <ispas.florian@gmail.com>
2023-08-22 19:37:15 +0300

    changes 5

--- src/test/TestGit.java
+++ src/test/TestGit.java
@@ -5,10 +5,11 @@ public class TestGit {
   //cod adaugat de coleg	
 	//cod adaugat local
   //cod adaugat de coleg 2	
-	//linie noua cod 3
 	//linie cod remote
 	//linie noua cod 4
 //linie cod remote 4
+	
+	//linie noua cod 5
 
 
 }
